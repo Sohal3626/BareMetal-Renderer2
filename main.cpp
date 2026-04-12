4c53511cc90698b6df5cb004c0925a9dea3acb3d
Sohal3626 <awamd3626@gmail.com>
2026-04-13 08:54:47 +0900

    chore : modify CMakeList.txt

--- main.cpp
+++ main.cpp
@@ -10,8 +10,8 @@
 
 // TIP 코드를 <b>Run</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나 여백에서 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 int main() {
-    constexpr int width = 1920;
-    constexpr int height = 1080;
+    constexpr int width = 300;
+    constexpr int height = 400;
     Canvas canvas(width, height);
 
     Mesh model;
@@ -19,14 +19,14 @@ int main() {
 
     // 3. 행렬 준비
     Mat44 modelMat;
-    translate(modelMat, 0, -0.7, 0);
+    translate(modelMat, 0, 0, 0);
     rotate (modelMat, 0, 0, 0);
 
     Mat44 viewMat;
 
-    translate(viewMat, 0, 0, -1.f);
+    translate(viewMat, 0, 0, -3.f);
     const Mat44 projectionMat =
-        perspective(90.0f, static_cast<float>(width)/static_cast<float>(height), 0.1f, 1000.0f);
+        perspective(45.0f, static_cast<float>(width)/static_cast<float>(height), 0.1f, 1000.0f);
 
     // 4. 버텍스 셰이더 생성
     const VertexShader vShader(modelMat, viewMat, projectionMat);
